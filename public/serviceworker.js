a6794b2c838a80307cf55b93794c6e4da19c9619
algmoreno <alg.moreno00@gmail.com>
2021-09-10 11:44:45 -0700

    updated files on service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -7,7 +7,14 @@ const FILES_TO_CACHE = [
   "./js/idb.js",
   './js/index.js',
   './css/styles.css',
-  './icons/icon-72x72'
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-382x382.png',
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('activate', function(e) {
